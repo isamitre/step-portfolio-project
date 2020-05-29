67ffe876c98828a4d1440fc13c027d425be5a199
Isabel Mitre <51170036+isamitre@users.noreply.github.com>
2020-05-29 16:25:02 -0400

    update script.js to have correct spacing

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -2,15 +2,14 @@ let modal = document.getElementById("myModal");
 let i;
 let img = document.getElementsByClassName("gallery");
 let modalImg = document.getElementById("img01");
- for(i=0;i< img.length;i++)
-   {    
-    img[i].onclick = function(){
 
+for(i=0;i< img.length;i++) {    
+  img[i].onclick = function() {
     modal.style.display = "block";
-       modalImg.src = this.src;
-
- }
+    modalImg.src = this.src;
+  }
 }
+
 let span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
    modal.style.display = "none";
