ba909f26d04da06a60a0a666cf82d8dea1184740
Isabel <isamitre@gmail.com>
2020-05-29 12:14:47 -0400

    adds pictures to secret-talents and sets up modal images in gallery

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,23 +1,17 @@
-console.log("check 0");
-// Get the modal
-var modal = document.getElementById('myModal');
-console.log("check 1");
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = $('.gallery');
-var modalImg = $("#img01");
-$('.gallery').click(function(){
+var modal = document.getElementById("myModal");
+var i;
+var img = document.getElementsByClassName("gallery");
+var modalImg = document.getElementById("img01");
+ for(i=0;i< img.length;i++)
+   {    
+    img[i].onclick = function(){
+
     modal.style.display = "block";
-    var newSrc = this.src;
-    modalImg.attr('src', newSrc);
-});
-console.log("check2");
+       modalImg.src = this.src;
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-console.log("check 3");
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+ }
 }
-console.log("check 4");
-
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+   modal.style.display = "none";
+}
