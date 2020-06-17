e4068667225ee54160394b400731e5690c45ad2d
Isabel <isamitre@gmail.com>
2020-06-17 16:36:58 +0000

    abstracted some functionality

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -28,125 +28,51 @@ public final class FindMeetingQuery {
     int requestSlots = (int) requestDuration / 30;
     int numEvents = events.size();
 
-    ArrayList<TimeRange> availableTimes = new ArrayList<TimeRange>();
-    ArrayList<Integer> availableSlots = new ArrayList<Integer>();
+    boolean[] mandatoryTimeSlots = new boolean[48];
+    boolean[] optionalTimeSlots = new boolean[48];
 
-    int[] timeSlots = new int[48];
-    Arrays.fill(timeSlots, 1);
+    ArrayList<TimeRange> possibleTimes = new ArrayList<TimeRange>();
+
 
     // if the duration is longer than a day means there should be no options.
     if (requestDuration >= TimeRange.WHOLE_DAY.duration() + 1)
       return Arrays.asList();
-    // if there are no mandatory attendees, the time range is the whole day
-    if (requestAttendees.size() == 0 && optionalAttendees.size() == 0) 
+    // if there are no attendees, the time range is the whole day
+    if (events.size() == 0 || requestAttendees.size() == 0 && optionalAttendees.size() == 0) 
       return Arrays.asList(TimeRange.WHOLE_DAY);
     
 
     // sets up timeSlots array; anywhere left true has all the requestAttendees available
     for (Event event : events) {
-      Set<String> mandatoryEventAttendees = event.getAttendees();
+      Set<String> eventAttendees = event.getAttendees();
       TimeRange when = event.getWhen();
-      int start = (when.start()) / 30;
-      int end = (when.end()) / 30;
-      int eventSlots = 0;
-      while (end > start) {
-        end--;
-        eventSlots++;
-      }    
-
-      int count = numEvents;
-      Iterator<String> eventAi = mandatoryEventAttendees.iterator(); 
-      while(eventAi.hasNext()) {
-        count--;
-        String eventAttendee = eventAi.next();
-        Iterator<String> optionalAi = optionalAttendees.iterator(); 
-        while(optionalAi.hasNext()) {
-          String optionalAttendee = optionalAi.next();
-          if (eventAttendee.equals(optionalAttendee)) {
-            for (int i = 0; i < eventSlots; i++) {
-              if(timeSlots[start + i] != -1)
-                timeSlots[start + i] = 0;
-            }
-            break;
-          }
-        }
-        start = (when.start()) / 30;
-        Iterator<String> requestAi = requestAttendees.iterator(); 
-        count = numEvents;
-        while(requestAi.hasNext()) {
-          String requestAttendee = requestAi.next();
-          if (eventAttendee.equals(requestAttendee)) {
-            for (int i = 0; i < eventSlots; i++) {
-              timeSlots[start + i] = -1;
-            }
-            break;
-          }
-        }
-        if(count == 0) {
-            break;
-        }
-      }
+ 
+      mandatoryTimeSlots = convertToTimeSlots(requestAttendees, events);
+      optionalTimeSlots = convertToTimeSlots(requestAttendees, events);    
     }
 
-    //checks the duration and how many time slots are necessary
-    if (Arrays.asList(timeSlots).contains(1)) {
-      boolean add;
-      for (int i = 0; i < 48; i++) {
-        add = false;
-        if (timeSlots[i] == 1) {
-          add = true;
-          // checks if the duration is longer than a single time slot
-          for (int j = 1; j < requestSlots; j++) {
-            if((i+j)<timeSlots.length && (timeSlots[i+j] == -1 || timeSlots[i+j] == 0)) {
-              add = false;
-            }
-          }
-        }
-        
-        if(add) {
-          for (int j = 0; j < requestSlots; j++) {
-            availableSlots.add(i+j);
-          }
-        }
-      }
-    }
-    if (availableSlots.size() < requestSlots) {
-      boolean add;
-      for (int i = 0; i < 48; i++) {
-        add = false;
-        if (timeSlots[i] == 0) {
-          add = true;
-          // checks if the duration is longer than a single time slot
-          for (int j = 1; j < requestSlots; j++) {
-            if((i+j)<timeSlots.length && timeSlots[i+j] == -1) {
-              add = false;
-            }
-          }
-        }
-        
-        if(add) {
-          for (int j = 0; j < requestSlots; j++) {
-            availableSlots.add(i+j);
-          }
-        }
-      }
-    }
+    ArrayList<Integer> possibleSlots = convertToSlots(mandatoryTimeSlots, requestSlots);
+    ArrayList<Integer> optionalSlots = convertToSlots(optionalTimeSlots, requestSlots);
+    // for(Integer i : possibleSlots) 
+    //     System.out.println(i);
+    
+    
     int start = TimeRange.START_OF_DAY;
     boolean done = false;
     // if there's just enough room
-    if (requestSlots == availableSlots.size()){
-      start = availableSlots.get(0)*30;
-      availableTimes.add(TimeRange.fromStartEnd(start, start + (int) requestDuration, false));
+    if (requestSlots == possibleSlots.size()){
+      start = possibleSlots.get(0)*30;
+      possibleTimes.add(TimeRange.fromStartEnd(start, start+ (int) requestDuration, false));
     }
     // everything else
-    for (int i = 0; i < availableSlots.size() - requestSlots; i++) {
+    for (int i = 0; i < possibleSlots.size() - requestSlots; i++) {
       // if the next value at the next index is not +1 from the last
-      if (availableSlots.get(i+1) - 1 != availableSlots.get(i)) {
-        availableTimes.add(TimeRange.fromStartEnd(start, availableSlots.get(i)*30+30, false));
-        start = availableSlots.get(i+1)*30;
+      if (possibleSlots.get(i+1) - 1 != possibleSlots.get(i)) {
+        possibleTimes.add(TimeRange.fromStartEnd(start, possibleSlots.get(i)*30+30, false));
+        start = possibleSlots.get(i+1)*30;
         numEvents--;
       }
-      if(i == availableSlots.size() - requestSlots - 1) {
+      if(i == possibleSlots.size() - requestSlots - 1) {
         done = true;
       }
       if(numEvents == 0){
@@ -155,9 +81,67 @@ public final class FindMeetingQuery {
       }
     }
     if(done) {
-      availableTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+      possibleTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
     }
 
-    return availableTimes;
+    return possibleTimes;
   }
+
+  public static boolean[] convertToTimeSlots (Collection<String> requestAttendees, Collection<Event> events) {
+    boolean[] timeSlots = new boolean[48];
+    Arrays.fill(timeSlots, Boolean.TRUE);
+    int numEvents = events.size();
+
+    for (Event event : events) {
+      Set<String> eventAttendees = event.getAttendees();
+      TimeRange when = event.getWhen();
+      int start = (when.start()) / 30;
+      int eventSlots = when.duration() / 30;
+
+      Iterator<String> eventAi = eventAttendees.iterator(); 
+      while(eventAi.hasNext()) {
+        numEvents--;
+        String eventAttendee = eventAi.next();
+
+        Iterator<String> requestAi = requestAttendees.iterator(); 
+        while(requestAi.hasNext()) {
+          String requestAttendee = requestAi.next();
+          if (eventAttendee.equals(requestAttendee)) {
+            for (int i = 0; i < eventSlots; i++) {
+              timeSlots[start + i] = false;
+            }
+            break;
+          }
+        }
+        if(numEvents == 0) {
+            break;
+        }
+      }
+    }
+    return timeSlots;
+  }
+
+  //converts the true/false values of timeSlots to possibleSlots
+  public static ArrayList<Integer> convertToSlots (boolean timeSlots[], int requestSlots) {
+    ArrayList<Integer> slots = new ArrayList<Integer>();
+    boolean add;
+    for (int i = 0; i < 48; i++) {
+      add = false;
+      if (timeSlots[i]) {
+        add = true;
+        // checks if the duration is longer than a single time slot
+        for (int j = 1; j < requestSlots; j++) {
+          if ((i+j) < timeSlots.length && !timeSlots[i+j]) {
+            add = false;
+          }
+        }
+      }
+      if(add) {
+        for (int j = 0; j < requestSlots; j++) {
+          slots.add(i+j);
+        }
+      }
+    }
+    return slots;
+    }
 }
