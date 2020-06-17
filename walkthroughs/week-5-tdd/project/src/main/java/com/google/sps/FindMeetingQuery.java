ec0d450186f275ca1b29bb97a9fd2f2d935759d9
Isabel <isamitre@gmail.com>
2020-06-17 21:16:52 +0000

    all tests but 2 optionalAttendees ones work

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -24,67 +24,39 @@ public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     Collection<String> requestAttendees = request.getAttendees();
     Collection<String> optionalAttendees = request.getOptionalAttendees();
+    Collection<String> allAttendees = new ArrayList<>();
+    allAttendees.addAll(requestAttendees);
+    allAttendees.addAll(optionalAttendees);
+    // allAttendees.addAll(requestAttendees);
     long requestDuration = request.getDuration();
-    int requestSlots = (int) requestDuration / 30;
+    int numRequestSlots = (int) requestDuration / 30;
     int numEvents = events.size();
 
     boolean[] mandatoryTimeSlots = new boolean[48];
-    boolean[] optionalTimeSlots = new boolean[48];
-
-    ArrayList<TimeRange> possibleTimes = new ArrayList<TimeRange>();
-
+    boolean[] allTimeSlots = new boolean[48];
 
     // if the duration is longer than a day means there should be no options.
     if (requestDuration >= TimeRange.WHOLE_DAY.duration() + 1)
       return Arrays.asList();
     // if there are no attendees, the time range is the whole day
-    if (events.size() == 0 || requestAttendees.size() == 0 && optionalAttendees.size() == 0) 
+    if (events.size() == 0 || allAttendees.size() == 0) 
       return Arrays.asList(TimeRange.WHOLE_DAY);
     
-
     // sets up timeSlots array; anywhere left true has all the requestAttendees available
-    for (Event event : events) {
-      Set<String> eventAttendees = event.getAttendees();
-      TimeRange when = event.getWhen();
- 
+    for (Event event : events) { 
       mandatoryTimeSlots = convertToTimeSlots(requestAttendees, events);
-      optionalTimeSlots = convertToTimeSlots(requestAttendees, events);    
+      allTimeSlots = convertToTimeSlots(allAttendees, events);    
     }
 
-    ArrayList<Integer> possibleSlots = convertToSlots(mandatoryTimeSlots, requestSlots);
-    ArrayList<Integer> optionalSlots = convertToSlots(optionalTimeSlots, requestSlots);
-    // for(Integer i : possibleSlots) 
-    //     System.out.println(i);
+    ArrayList<Integer> possibleSlots = convertToSlots(mandatoryTimeSlots, numRequestSlots);
+    ArrayList<Integer> allSlots = convertToSlots(allTimeSlots, numRequestSlots);    
     
+    ArrayList<TimeRange> possibleTimes = convertToTimes(possibleSlots, numRequestSlots, numEvents);
+    ArrayList<TimeRange> allTimes = convertToTimes(allSlots, numRequestSlots, numEvents);
     
-    int start = TimeRange.START_OF_DAY;
-    boolean done = false;
-    // if there's just enough room
-    if (requestSlots == possibleSlots.size()){
-      start = possibleSlots.get(0)*30;
-      possibleTimes.add(TimeRange.fromStartEnd(start, start+ (int) requestDuration, false));
-    }
-    // everything else
-    for (int i = 0; i < possibleSlots.size() - requestSlots; i++) {
-      // if the next value at the next index is not +1 from the last
-      if (possibleSlots.get(i+1) - 1 != possibleSlots.get(i)) {
-        possibleTimes.add(TimeRange.fromStartEnd(start, possibleSlots.get(i)*30+30, false));
-        start = possibleSlots.get(i+1)*30;
-        numEvents--;
-      }
-      if(i == possibleSlots.size() - requestSlots - 1) {
-        done = true;
-      }
-      if(numEvents == 0){
-        done = true;
-        break;
-      }
-    }
-    if(done) {
-      possibleTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
-    }
-
-    return possibleTimes;
+    if(allTimes.isEmpty())
+        return possibleTimes;
+    return allTimes;
   }
 
   public static boolean[] convertToTimeSlots (Collection<String> requestAttendees, Collection<Event> events) {
@@ -122,7 +94,7 @@ public final class FindMeetingQuery {
   }
 
   //converts the true/false values of timeSlots to possibleSlots
-  public static ArrayList<Integer> convertToSlots (boolean timeSlots[], int requestSlots) {
+  public static ArrayList<Integer> convertToSlots (boolean timeSlots[], int numRequestSlots) {
     ArrayList<Integer> slots = new ArrayList<Integer>();
     boolean add;
     for (int i = 0; i < 48; i++) {
@@ -130,18 +102,50 @@ public final class FindMeetingQuery {
       if (timeSlots[i]) {
         add = true;
         // checks if the duration is longer than a single time slot
-        for (int j = 1; j < requestSlots; j++) {
+        for (int j = 1; j < numRequestSlots; j++) {
           if ((i+j) < timeSlots.length && !timeSlots[i+j]) {
             add = false;
           }
         }
       }
       if(add) {
-        for (int j = 0; j < requestSlots; j++) {
+        for (int j = 0; j < numRequestSlots; j++) {
           slots.add(i+j);
         }
       }
     }
     return slots;
+  }
+
+  public static ArrayList<TimeRange> convertToTimes (ArrayList<Integer> slots, int numRequestSlots, int numEvents) {
+    ArrayList<TimeRange> possibleTimes = new ArrayList<TimeRange>();
+    int start = TimeRange.START_OF_DAY;
+    boolean done = false;
+
+    // if there's just enough room
+    if (numRequestSlots == slots.size()){
+      start = slots.get(0)*30;
+      possibleTimes.add(TimeRange.fromStartEnd(start, start + numRequestSlots*30, false));
     }
+    // everything else
+    for (int i = 0; i < slots.size() - numRequestSlots; i++) {
+      // if the next value at the next index is not +1 from the last
+      if (slots.get(i+1) - 1 != slots.get(i)) {
+        possibleTimes.add(TimeRange.fromStartEnd(start, slots.get(i)*30+30, false));
+        start = slots.get(i+1)*30;
+        numEvents--;
+      }
+      if(i == slots.size() - numRequestSlots - 1) {
+        done = true;
+      }
+      if(numEvents == 0){
+        done = true;
+        break;
+      }
+    }
+    if(done) {
+      possibleTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+    }
+    return possibleTimes;
+  }
 }
